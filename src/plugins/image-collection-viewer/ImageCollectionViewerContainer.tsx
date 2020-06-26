a3964a9f13b6b19506b132cfce7b7c8610575bfd
Dhanesh Neela Mana <dhanesh.neelamana@epfl.ch>
2020-06-26 15:26:34 +0200

    Show images as List
    - Show images as list.
    - Add a dropdown to filter images by type.
    
    Change-Id: I5f1cbed4b1a215a2c85e4c0d314e29933be5e05f

--- src/plugins/image-collection-viewer/ImageCollectionViewerContainer.tsx
+++ src/plugins/image-collection-viewer/ImageCollectionViewerContainer.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import { Resource, NexusClient, NexusFile } from '@bbp/nexus-sdk';
+import { List, Popover, Card, Spin, Menu, Dropdown } from 'antd';
+import 'antd/dist/antd.css';
+import { ClickParam } from 'antd/lib/menu';
 
-import ImageCollectionViewerComponent, {
-  ImageCollection,
-} from './ImageCollectionViewerComponent';
+type ImageCollection = {
+  imageSrc: string;
+  name?: string;
+}[];
 
 const ImageCollectionViewerContainer: React.FC<{
   resource: Resource;
@@ -19,6 +23,51 @@ const ImageCollectionViewerContainer: React.FC<{
     data: null,
   });
 
+  const memoizedSTypes = React.useMemo(() => {
+    const t = Array.isArray(resource.image)
+      ? resource.image.map(({ stimulusType: sType }) => {
+          if (sType) {
+            const typeString = sType['@id'].split('/');
+            return typeString[typeString.length - 1];
+          }
+          return '';
+        })
+      : [];
+    // remove duplicates.
+    const returnArray = [...new Set(t)];
+    // remove empty strings, if any.
+    returnArray.splice(returnArray.indexOf(''), 1);
+    return returnArray;
+  }, [resource]);
+
+  const [selectedType, setSelectedType] = React.useState<String>('All');
+  const filteredData = React.useMemo(() => {
+    if (selectedType === 'All') {
+      return data;
+    }
+    const selectedImages = Array.isArray(resource.image)
+      ? resource.image.filter(i => {
+          const typeString = i.stimulusType['@id'].split('/');
+          return typeString[typeString.length - 1] === selectedType;
+        })
+      : [resource.image];
+    const selectedImageIds = selectedImages.map(i => i['@id']);
+    return data?.filter(d => selectedImageIds.includes(d.name));
+  }, [resource, data, selectedType]);
+
+  const menu = (
+    <Menu
+      onClick={(param: ClickParam) => {
+        setSelectedType(param.key);
+      }}
+    >
+      <Menu.Item key={'All'}>All</Menu.Item>
+      {memoizedSTypes.map(v => (
+        <Menu.Item key={v}>{v}</Menu.Item>
+      ))}
+    </Menu>
+  );
+
   React.useEffect(() => {
     if (!resource.image) {
       throw new Error('No Image Collection Property Found');
@@ -27,7 +76,6 @@ const ImageCollectionViewerContainer: React.FC<{
     const [projectLabel, orgLabel, ...rest] = resource._project
       .split('/')
       .reverse();
-
     const promises = (Array.isArray(resource.image)
       ? resource.image
       : [resource.image]
@@ -51,7 +99,6 @@ const ImageCollectionViewerContainer: React.FC<{
           console.warn(`${resource['@id']} is not an image File`);
           return null;
         }
-
         if (
           !(resource as NexusFile)._mediaType.includes('image') &&
           (resource as NexusFile)._bytes <= MAX_BYTES_TO_PREVIEW
@@ -99,7 +146,65 @@ const ImageCollectionViewerContainer: React.FC<{
     };
   }, [resource['@id']]);
 
-  return <ImageCollectionViewerComponent {...{ loading, error, data }} />;
+  if (error) {
+    return <>{error.message}</>;
+  }
+  if (loading) {
+    return (
+      <div className="image-collection-viewer">
+        <div className="wrapper -loading">
+          <div className="skeleton">
+            <Spin />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (filteredData) {
+    return (
+      <div className="image-collection-viewer">
+        <Dropdown overlay={menu}>
+          <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+            {selectedType}
+          </a>
+        </Dropdown>
+        <List
+          grid={{
+            gutter: 12,
+            column: 3,
+          }}
+          dataSource={filteredData}
+          renderItem={item => {
+            const content = (
+              <Card
+                hoverable
+                style={{ width: '700px' }}
+                cover={<img alt={item.name} src={item.imageSrc} />}
+              >
+                <a download href={item.imageSrc} title={item.name}>
+                  Download
+                </a>
+              </Card>
+            );
+            return (
+              <List.Item>
+                <Popover content={content} trigger="click">
+                  <img
+                    alt={item.name}
+                    src={item.imageSrc}
+                    style={{ width: '200px', cursor: 'pointer' }}
+                  />
+                </Popover>
+              </List.Item>
+            );
+          }}
+        />
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default ImageCollectionViewerContainer;
